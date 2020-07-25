d2d67793416366f7d28a979b3f94dac549b98e99
Eric Liu <sqfkw1578@gmail.com>
2020-07-26 07:57:58 +0800

    新增@babel/runtime、@babel/plugin-transform-runtime

--- webpack/scripts/typescript__babel-loader__react__styled-jsx.js
+++ webpack/scripts/typescript__babel-loader__react__styled-jsx.js
@@ -2,21 +2,34 @@
 module.exports = {
   install: function () {
     require('../helpers/install')(['react', 'react-dom', '@babel/runtime'])
-    require('../helpers/install-D')(['@babel/core', 'babel-loader', '@babel/preset-env', '@babel/preset-react', 'styled-jsx', '@babel/plugin-transform-runtime'])
+    require('../helpers/install-D')([
+      'babel-loader',
+      '@babel/core',
+      '@babel/preset-typescript',
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      'styled-jsx',
+      '@babel/plugin-transform-runtime',
+    ])
   },
   config: function () {
-    global.config.module.rules.push({
-      test: /\.js(x?)$/,
+    config.module.rules.push({
+      test: /\.ts(x?)$/,
       exclude: /node_modules/,
       use: [
         {
           loader: 'babel-loader',
           options: {
             presets: [
+              '@babel/preset-typescript',
               '@babel/preset-env',
               '@babel/preset-react',
             ],
             plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-object-rest-spread',
               'styled-jsx/babel',
               '@babel/plugin-transform-runtime',
             ],
